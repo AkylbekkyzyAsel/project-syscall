5abe7919323e6dbfd3ba8bd65a54a429bdcddc2c
AkylbekkyzyAsel <44228267+AkylbekkyzyAsel@users.noreply.github.com>
2020-10-25 21:02:01 +0600

    Update ish_syscalls.riscv64.c

--- ish/ish_syscalls.riscv64.c
+++ ish/ish_syscalls.riscv64.c
@@ -4,21 +4,46 @@ long ish_read(
         unsigned long buffer_size
      )
 {
-    // TODO
+         register long result asm("a0");
 
-    return -1;
+        __asm__ __volatile__ (
+                "li $a7, 63\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
+
+        return result;
 }
 
 int ish_chdir(const char *path)
 {
-    // TODO
+         register int result asm("a0");
 
-    return -1;
+        __asm__ __volatile__ (
+                "li $a7, 49\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
+
+        return result;
 }
 
 void ish_exit(int status)
 {
-    // TODO
+         register long result asm("a0");
+
+        __asm__ __volatile__ (
+                "li $a7, 93\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
+
 }
 
 int ish_stat(const char *path, void *stat_result)
@@ -37,23 +62,45 @@ int ish_open(const char *path, int flags)
 
 int ish_creat(const char *path, unsigned int mode)
 {
-    // TODO
+        register int result asm("a0");
 
-    return -1;
+        __asm__ __volatile__ (
+                "li $a7, 56\n\t"
+                "li $a2, 577\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
 }
 
 int ish_dup2(int old_file_descriptor, int new_file_descriptor)
 {
-    // TODO
+         register int result asm("a0");
 
-    return -1;
+        __asm__ __volatile__ (
+                "li $a7, 24\n\t"
+                "li $a2, 0\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
 }
 
 int ish_close(int file_descriptor)
 {
-    // TODO
 
-    return -1;
+         register int result asm("a0");
+
+        __asm__ __volatile__ (
+                "li $a7, 57\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
+
 }
 
 int ish_fork()
@@ -69,16 +116,30 @@ int ish_execve(
         char *const environment[]
     )
 {
-    // TODO
 
-    return -1;
+         register int result asm("a0");
+
+        __asm__ __volatile__ (
+                "li $a7, 221\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
 }
 
 int ish_waitpid(int pid, int *status, int options)
 {
-    // TODO
+         register int result asm("a0");
 
-    return -1;
+        __asm__ __volatile__ (
+                "li $a7, 221\n\t"
+                "li $a3, 0\n\n"                
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
 }
 
 long ish_write(
@@ -87,7 +148,13 @@ long ish_write(
         unsigned long buffer_size
      )
 {
-    // TODO
+         register long result asm("a0");
 
-    return -1;
+        __asm__ __volatile__ (
+                "li $a7, 64\n\t"
+                "ecall"
+                : "=r" (result)
+                :
+		:"$a7"
+        );
 }
